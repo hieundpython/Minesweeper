cca9291f88222deaa456962440c33e7fc847a6d5
hieund <nguyen.duc.hieu@gosei.com.vn>
2020-11-04 17:16:10 +0700

    view cell and bees

---
+++ sketch.js
@@ -0,0 +1,60 @@
+function make2DArray(cols, rows) {
+  var arr = new Array(cols);
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+  return arr;
+}
+
+var grid;
+
+var cols;
+var rows;
+var w = 40;
+var totalBees = 20;
+
+function setup() {
+  createCanvas(400, 400);
+  cols = floor(width / w);
+  rows = floor(height / w);
+  grid = make2DArray(cols, rows);
+
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j] = new Cell(i, j, w);
+    }
+  }
+
+  for (var n = 0; n < totalBees; n++) {
+    var i = floor(random(cols));
+    var j = floor(random(rows));
+
+    grid[i][j].bee = true;
+  }
+
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j].countBees();
+    }
+  }
+}
+
+function mousePressed() {
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      if (grid[i][j].contains(mouseX, mouseY)) {
+        grid[i][j].reveal();
+      }
+    }
+  }
+}
+
+function draw() {
+  background(255);
+
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j].show();
+    }
+  }
+}
